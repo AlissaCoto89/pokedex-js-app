c6f7e945b2e66cb7137e9db61494ca0c19dfd2a6
AlissaCoto89 <acoto189@gmail.com>
2022-07-28 16:37:26 -0400

    added conditionals for weight

--- js/script.js
+++ js/script.js
@@ -20,8 +20,13 @@ let pokemonList = [
 		types: ['grass' , 'poison']}
 ];
 
-//create for loop that iterates for Pokemons name and height
+//create for loop that iterates for Pokemons name and weight
 
-for (let i = 0; i < pokemonList.length; i++) {
-	document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") ")}
-	
+for (let i = 0; i < pokemonList.length; i++) { //conditionals added for weight
+	if (pokemonList[i].weight > 40) {
+		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Wow, that\'s big!')
+	} else if (pokemonList[i].weight <= 40 && pokemonList[i].weight > 20) {
+		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - That\'s a normal size.')
+	} else	if (pokemonList[i].weight < 20) {
+		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Awe, that\'s tiny!')
+	}}
