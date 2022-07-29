05ee5a8a9c36e96c0b224805d8e2c3079c2feee0
AlissaCoto89 <acoto189@gmail.com>
2022-07-29 13:44:22 -0400

    make notes on codes action

--- js/script.js
+++ js/script.js
@@ -23,10 +23,10 @@ let pokemonList = [
 //create for loop that iterates for Pokemons name and weight
 
 for (let i = 0; i < pokemonList.length; i++) { //conditionals added for weight
-	if (pokemonList[i].weight > 40) {
+	if (pokemonList[i].weight > 40) { //If Pokemon weighs over 40 declares wow thats big
 		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Wow, that\'s big!')
-	} else if (pokemonList[i].weight <= 40 && pokemonList[i].weight > 20) {
+	} else if (pokemonList[i].weight <= 40 && pokemonList[i].weight > 20) { //If Pokemon is between 20 and 40 declares thats a decent size
 		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - That\'s a decent size.')
-	} else	if (pokemonList[i].weight < 20) {
+	} else	if (pokemonList[i].weight < 20) { //if Pokemon weighs under 20 declares awe thats tiny
 		document.write('<p>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Awe, that\'s tiny!')
 	}}
