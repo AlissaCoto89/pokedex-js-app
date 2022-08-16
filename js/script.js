aa9d208283b8a325b14cdbc3c3e7cd5f4dd8dbf6
AlissaCoto89 <acoto189@gmail.com>
2022-08-16 17:16:56 -0400

    created a modal withpokemon images

--- js/script.js
+++ js/script.js
@@ -1,6 +1,118 @@
 let pokemonRepository = (function () {
 let pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+let modalContainer = document.querySelector('#modal-container');
+
+function showModal(title, text) {
+	modalContainer.innerHTML = '';
+	let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+      
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+	modalContainer.classList.add('is-visible');
+}
+
+function showModalImage(title, text, vurlimag) {
+    modalContainer.innerHTML = '';
+	let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+      
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    let imageElement = document.createElement('img');
+    imageElement.src = vurlimag;
+
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+	modalContainer.classList.add('is-visible');
+}  
+
+let dialogPromiseReject;
+
+function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+
+    if (dialogPromiseReject) {
+    	dialogPromiseReject();
+    	dialogPromiseReject = null;
+    }
+}
+
+function showDialog(title, text) {
+	showModal(title, text);
+
+    let modalContainer = document.querySelector('#modal-container');
+    let modal = modalContainer.querySelector('.modal');
+	let confirmButton = document.createElement('button');
+    confirmButton.classList.add('modal-confirm');
+    confirmButton.innerText = 'Confirm';
+
+    let cancelButton = document.createElement('button');
+    cancelButton.classList.add('modal-cancel');
+    cancelButton.innerText = 'Cancel';
+
+    modal.appendChild(confirmButton);
+    modal.appendChild(cancelButton);
+
+    
+    confirmButton.focus();
+    
+    return new Promise((resolve, reject) => {
+    	cancelButton.addEventListener('click', hideModal);
+    	confirmButton.addEventListener('click', () => {
+        dialogPromiseReject = null; 
+        hideModal();
+        resolve();
+    });
+
+    dialogPromiseReject = reject;
+    });
+}
+
+
+
+window.addEventListener('keydown', (e) => {
+	if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    	hideModal();  
+      }
+    });
+
+modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+    	hideModal();
+      }
+    });
 
 function add(pokemon){
 	if (
@@ -65,8 +177,9 @@ function loadDetails(item) {
 }
 
 function showDetails(pokemon){
-	pokemonRepository.loadDetails(pokemon).then(function () {
-    console.log(pokemon);
+	loadDetails(pokemon).then(function () {
+	showModalImage(pokemon.name, pokemon.height, pokemon.imageUrl, pokemon.weight, pokemon.types);
+    //console.log(pokemon);
 	});
 }
 
@@ -74,6 +187,7 @@ function showDetails(pokemon){
 return {
     add: add,
     getAll: getAll,
+    showModal: showModal,
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
@@ -83,8 +197,9 @@ return {
 
 
 pokemonRepository.loadList().then(function () {
+	//pokemonRepository.showAll();
     pokemonRepository.getAll().forEach(function (pokemon) {
-        pokemonRepository.addListItem(pokemon);
+    pokemonRepository.addListItem(pokemon);
     });
 
 });
