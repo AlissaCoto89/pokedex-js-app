5a988b2f49de4d9441217ff8962fc92826dda9d8
AlissaCoto89 <acoto189@gmail.com>
2022-08-23 15:01:56 -0400

    added comments

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
   let modalContainer = document.querySelector("#modal-container");
-
+  //Modal
   function showModal(pokemon) {
     let modalBody = $(".modal-body");
     let modalTitle = $(".modal-title");
 
     modalTitle.empty();
     modalBody.empty();
-
+    //declare Name, image, height, weight, type, and abilities
     let nameElement = $("<h3>" + pokemon.name + "</h3>");
     let imageElement = $('<img class="pokemon-img">');
     imageElement.attr("src", pokemon.imageUrl);
@@ -19,7 +19,7 @@ let pokemonRepository = (function () {
     let abilitiesElement = $(
       "<p>" + "Abilities: " + pokemon.abilities + "</p>"
     );
-
+    //add stats to modal
     modalTitle.append(nameElement);
     modalBody.append(imageElement);
     modalBody.append(heightElement);
@@ -54,7 +54,6 @@ let pokemonRepository = (function () {
     let listItem = document.createElement("li");
     let pokemonItem = document.createElement("li");
     pokemonList.classList.add("group-list-item");
-    pokemonList.classList.add("col-sm-4", "col-md-6", "col-lg-12");
     let buttonItem = document.createElement("button");
     buttonItem.classList.add("pokemonButton");
     buttonItem.innerText = pokemon.name;
@@ -80,7 +79,6 @@ let pokemonRepository = (function () {
             detailsUrl: item.url,
           };
           add(pokemon);
-          //console.log(pokemon);
         });
       })
       .catch(function (e) {
@@ -95,7 +93,7 @@ let pokemonRepository = (function () {
         return response.json();
       })
       .then(function (details) {
-        //details added to the item
+        //details added to the pokemon
         pokemon.imageUrl = details.sprites.front_default;
         pokemon.height = details.height;
         pokemon.types = details.types.map((type) => type.type.name).join(", ");
@@ -112,7 +110,6 @@ let pokemonRepository = (function () {
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       showModal(pokemon);
-      //console.log(pokemon);
     });
   }
 
@@ -128,7 +125,6 @@ let pokemonRepository = (function () {
 })();
 
 pokemonRepository.loadList().then(function () {
-  //pokemonRepository.showAll();
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
